4936e17c1cfdd0dbb041a2bac3a51b0439bf6bfe
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-07-28 01:53:52 -0300

    add(config): constants added

--- Source/Core/config.php
+++ Source/Core/config.php
@@ -1,12 +1,12 @@
 <?php
 
-const CONF_BASE_URL = "https://yu-gi-oh-crud/";
+const CONF_BASE_URL = "https://krcp";
 
 const CONF_DEFAULT_CONTROLLER = "Home:index";
 
-const CONF_DB = [
-    "host" => "localhost",
-    "user" => "josafaverissimo",
-    "name" => "yugioh",
-    "password" => "root"
-];
+const CONF_DB_HOST = "localhost";
+const CONF_DB_USER = "josafaverissimo";
+const CONF_DB_NAME = "krcp";
+const CONF_DB_PASSWORD = "root";
+
+const CONF_HTML_DEFAULT_ICON = "public/assets/imgs/page-icon.jpeg";
