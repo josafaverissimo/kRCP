3857c172efa66d7c29816b63af0ef7ba1c38f2c4
Josafá Veríssimo Gomes <josafaverissimo98@gmail.com>
2023-07-31 08:03:05 -0300

    add(js): js scripts added

---
+++ public/assets/js/user/form.js
@@ -0,0 +1,23 @@
+"use strict";
+async function sendData(formData, target) {
+    const response = await fetch(target, {
+        method: "post",
+        body: formData
+    });
+    return await response.json();
+}
+document.addEventListener("DOMContentLoaded", () => {
+    const mainForm = document.getElementById("main-form");
+    if (!mainForm) {
+        return;
+    }
+    mainForm.addEventListener("submit", event => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append("username", mainForm.username.value);
+        formData.append("password", mainForm.password.value);
+        sendData(formData, mainForm.action).then(json => {
+            console.log(json);
+        });
+    });
+});
